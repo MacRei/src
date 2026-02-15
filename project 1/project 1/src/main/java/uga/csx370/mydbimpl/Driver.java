661663161765fc18fbc061b10dbf41d208f8df41
MacguireR <msr00109@uga.edu>
2026-02-15 12:49:57 -0500

    Added my query and moved all resource files into one folder

--- project 1/project 1/src/main/java/uga/csx370/mydbimpl/Driver.java
+++ project 1/project 1/src/main/java/uga/csx370/mydbimpl/Driver.java
@@ -8,9 +8,6 @@ package uga.csx370.mydbimpl;
 
 import java.util.List;
 
-import uga.csx370.mydb.Relation;
-import uga.csx370.mydb.RelationBuilder;
-import uga.csx370.mydb.Type;
 import uga.csx370.mydb.*;
 import uga.csx370.mydbimpl.RAImpl;
 
@@ -18,29 +15,57 @@ public class Driver {
     
     public static void main(String[] args) {
         
-        // myID
-        System.out.println("myid: 811705713");
-        // relation 1 (instructor table)
+        //// myID
+        //System.out.println("myid: 811705713");
+        //// relation 1 (instructor table)
+        //Relation rel1 = new RelationBuilder()
+        //        .attributeNames(List.of("ID", "name", "dept_name", "salary"))
+        //        .attributeTypes(List.of(Type.STRING, Type.STRING, Type.STRING, Type.DOUBLE))
+        //        .build();
+        //// load data from CSV file
+        //rel1.loadData("exported_data/instructor_export.csv"); //instructor_export_small if only want first 10 entries
+        //// print the relation
+        //rel1.print();
+//
+        //// myID
+        //System.out.println("myid: 811705713");
+        //// relation 2 (student table)
+        //Relation rel2 = new RelationBuilder()
+        //        .attributeNames(List.of("ID", "name", "dept_name", "tot_cred"))
+        //        .attributeTypes(List.of(Type.STRING, Type.STRING, Type.STRING, Type.INTEGER))
+        //        .build();
+        //// load data from CSV file
+        //rel2.loadData("exported_data/student_export.csv"); //student_export_small if only want first 10 entries
+        //// print the relation
+        //rel2.print();
+//
+        RA ra = new RAImpl();
+        
+
         Relation rel1 = new RelationBuilder()
-                .attributeNames(List.of("ID", "name", "dept_name", "salary"))
+                .attributeNames(List.of("courseID", "name", "dept_name", "credits"))
                 .attributeTypes(List.of(Type.STRING, Type.STRING, Type.STRING, Type.DOUBLE))
                 .build();
-        // load data from CSV file
-        rel1.loadData("exported_data/instructor_export.csv"); //instructor_export_small if only want first 10 entries
-        // print the relation
-        rel1.print();
+        rel1.loadData("project 1/project 1/exported_data/course.csv");
 
-        // myID
-        System.out.println("myid: 811705713");
-        // relation 2 (student table)
         Relation rel2 = new RelationBuilder()
-                .attributeNames(List.of("ID", "name", "dept_name", "tot_cred"))
-                .attributeTypes(List.of(Type.STRING, Type.STRING, Type.STRING, Type.INTEGER))
-                .build();
-        // load data from CSV file
-        rel2.loadData("exported_data/student_export.csv"); //student_export_small if only want first 10 entries
-        // print the relation
-        rel2.print();
-    }
+        .attributeNames(List.of("course_ID", "prereq_ID"))
+        .attributeTypes(List.of(Type.STRING, Type.STRING))
+        .build();
+        rel2.loadData("project 1/project 1/exported_data/prereq.csv");
 
+        Relation joined = ra.join(rel1, rel2, row ->
+                row.get(0).toString().equals(row.get(4).toString())
+        );
+
+        joined = ra.project(joined, List.of("course_ID", "name", "prereq_ID"));
+        joined = ra.rename(joined, List.of("course_ID", "name", "prereq_ID"), List.of("course_ID", "courseName", "prereq_ID"));
+        joined = ra.join(joined, rel1, row ->
+                row.get(2).toString().equals(row.get(3).toString())
+        );
+        joined = ra.project(joined, List.of("course_ID", "courseName", "prereq_ID", "name"));
+        joined = ra.rename(joined, List.of("course_ID", "courseName", "prereq_ID", "name"), List.of("course_ID", "course_name", "prereq_ID", "prereq_name"));
+        joined.print();
+    }
+    
 }
