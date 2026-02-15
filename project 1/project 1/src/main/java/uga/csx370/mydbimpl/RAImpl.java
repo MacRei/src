9e4e5701863ff873dcb52a45cf29c16cc6965ea2
EvanM42 <evanmccann42@gmail.com>
2026-02-15 09:42:54 -0500

    pushed so I can pull from main

--- project 1/project 1/src/main/java/uga/csx370/mydbimpl/RAImpl.java
+++ project 1/project 1/src/main/java/uga/csx370/mydbimpl/RAImpl.java
@@ -1,21 +1,27 @@
 package uga.csx370.mydbimpl;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import uga.csx370.mydb.Cell;
 import uga.csx370.mydb.Predicate;
 import uga.csx370.mydb.RA;
 import uga.csx370.mydb.Relation;
+import uga.csx370.mydb.RelationBuilder;
+import uga.csx370.mydb.Type;
 
 public class RAImpl implements RA {
 
     // need to do
     public Relation select(Relation rel, Predicate p) {
-        // TODO Auto-generated method stub
-        // throw new UnsupportedOperationException("Unimplemented method 'select'");
-        Relation result = new Relation(rel.getAttributeNames(), rel.getAttributeTypes());
-        for (List<Object> tuple : rel.getTuples()) {
-            if (p.evaluate(tuple)) {
-                result.addTuple(tuple);
+        Relation result = new RelationBuilder()
+                .attributeNames(rel.getAttrs())
+                .attributeTypes(rel.getTypes())
+                .build();
+        for (int i = 0; i < rel.getSize(); i++) {
+            List<Cell> row = rel.getRow(i);
+            if (p.check(row)) {
+                result.insert(row);
             }
         }
         return result;
@@ -23,16 +29,31 @@ public class RAImpl implements RA {
 
     // need to do
     public Relation project(Relation rel, List<String> attrs) {
-        // TODO Auto-generated method stub
-        // throw new UnsupportedOperationException("Unimplemented method 'project'");
-        Relation result = new Relation(attrs, rel.getAttributeTypes());
-        for (List<Object> tuple : rel.getTuples()) {
-            List<Object> projectedTuple = new ArrayList<>();
-            for (String attr : attrs) {
-                int index = rel.getAttributeNames().indexOf(attr);
-                projectedTuple.add(tuple.get(index));
+        // validate attributes exist
+        for (String attr : attrs) {
+            if (!rel.hasAttr(attr)) {
+                throw new IllegalArgumentException("Attribute not found: " + attr);
             }
-            result.addTuple(projectedTuple);
+        }
+        // build projected types list
+        List<Type> projectedTypes = new ArrayList<>();
+        List<Integer> indices = new ArrayList<>();
+        for (String attr : attrs) {
+            int idx = rel.getAttrIndex(attr);
+            indices.add(idx);
+            projectedTypes.add(rel.getTypes().get(idx));
+        }
+        Relation result = new RelationBuilder()
+                .attributeNames(attrs)
+                .attributeTypes(projectedTypes)
+                .build();
+        for (int i = 0; i < rel.getSize(); i++) {
+            List<Cell> row = rel.getRow(i);
+            List<Cell> projectedRow = new ArrayList<>();
+            for (int idx : indices) {
+                projectedRow.add(row.get(idx));
+            }
+            result.insert(projectedRow);
         }
         return result;
     }
@@ -70,14 +91,51 @@ public class RAImpl implements RA {
 
     // need to do
     public Relation join(Relation rel1, Relation rel2) {
-        // TODO Auto-generated method stub
-        // throw new UnsupportedOperationException("Unimplemented method 'join'");
-        Relation result = new Relation(rel.getAttributeNames(), rel.getAttributeTypes());
-        for (List<Object> tuple1 : rel1.getTuples()) {
-            for (List<Object> tuple2 : rel2.getTuples()) {
-                List<Object> joinedTuple = new ArrayList<>(tuple1);
-                joinedTuple.addAll(tuple2);
-                result.addTuple(joinedTuple);
+        // find common attributes
+        List<String> commonAttrs = new ArrayList<>();
+        for (String attr : rel1.getAttrs()) {
+            if (rel2.hasAttr(attr)) {
+                commonAttrs.add(attr);
+            }
+        }
+        // build result attribute list: all of rel1, then rel2 attrs that aren't common
+        List<String> resultAttrs = new ArrayList<>(rel1.getAttrs());
+        List<Type> resultTypes = new ArrayList<>(rel1.getTypes());
+        List<Integer> rel2NonCommonIndices = new ArrayList<>();
+        for (int i = 0; i < rel2.getAttrs().size(); i++) {
+            String attr = rel2.getAttrs().get(i);
+            if (!commonAttrs.contains(attr)) {
+                resultAttrs.add(attr);
+                resultTypes.add(rel2.getTypes().get(i));
+                rel2NonCommonIndices.add(i);
+            }
+        }
+        Relation result = new RelationBuilder()
+                .attributeNames(resultAttrs)
+                .attributeTypes(resultTypes)
+                .build();
+        // perform the natural join
+        for (int i = 0; i < rel1.getSize(); i++) {
+            List<Cell> row1 = rel1.getRow(i);
+            for (int j = 0; j < rel2.getSize(); j++) {
+                List<Cell> row2 = rel2.getRow(j);
+                // check if common attributes match
+                boolean match = true;
+                for (String attr : commonAttrs) {
+                    Cell val1 = row1.get(rel1.getAttrIndex(attr));
+                    Cell val2 = row2.get(rel2.getAttrIndex(attr));
+                    if (!val1.equals(val2)) {
+                        match = false;
+                        break;
+                    }
+                }
+                if (match) {
+                    List<Cell> joinedRow = new ArrayList<>(row1);
+                    for (int idx : rel2NonCommonIndices) {
+                        joinedRow.add(row2.get(idx));
+                    }
+                    result.insert(joinedRow);
+                }
             }
         }
         return result;
